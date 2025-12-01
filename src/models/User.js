cc88fd849c8c08f7ad9e3ebde040be2e28ddea1c
Vybyranyi <mariankvit@gmail.com>
2025-12-01 20:06:27 +0200

    feat: Add manager and developer roles to User model enum.

--- src/models/User.js
+++ src/models/User.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ['admin', 'user'],
+        enum: ['admin', 'manager', 'developer', 'user'],
         default: 'user',
     },
     resetToken: {
